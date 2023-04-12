82c86976d3fbac5156e0c7f5f7f4998d4c0ad652
UrielLoreto <uriel.loreto8@gmail.com>
2023-04-11 22:35:49 -0600

    add redirect page

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -1,31 +1,28 @@
 import React from "react";
 import logotipo from "../../images/logotipo.jpeg";
-import {Link} from "react-router-dom";
 
-export const Header =() => {
-    return (
-        <header>
-            <a href="#">
-                <div className="logo">
-                    <img src= {logotipo} alt="logo" width="150"/>
-                </div>
-            </a>
-            <ul>
-                <li>
-                  <a href="#" >INICIO</a>
-                  </li>
-            </ul>
-            <ul>
-                <li>
-                  <a href="#" >PRODUCTOS</a>
-                  </li>
-            </ul>
-            <div className="cart">
-                <box-icon name="cart"></box-icon>
-                <span className="item_total">0</span>
-            </div>
-
-        </header>
-
-    )
-}
+export const Header = () => {
+  return (
+    <header>
+      <a href="#">
+        <div className="logo">
+          <img src={logotipo} alt="logo" width="150" />
+        </div>
+      </a>
+      <ul>
+        <li>
+          <a href="/">INICIO</a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a href="productos">PRODUCTOS</a>
+        </li>
+      </ul>
+      <div className="cart">
+        <box-icon name="cart"></box-icon>
+        <span className="item_total">0</span>
+      </div>
+    </header>
+  );
+};
