e87dcab247bd6f5243de1552755bb0d1b1c26e89
IvonneAmezola <71673298+IvonneAmezola@users.noreply.github.com>
2023-04-12 10:46:50 -0600

    prueba

--- src/componentes/Inicio/index.js
+++ src/componentes/Inicio/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Inicio = () => {
   return (
     <>
-      <h1 className="title">Inicio</h1>
+      <h1 className="title">cambio Inicio</h1>
     </>
   );
 };
