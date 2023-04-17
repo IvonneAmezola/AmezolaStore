9a7d1c226107b250c4470dd38429dfbf8f0754ad
IvonneAmezola <71673298+IvonneAmezola@users.noreply.github.com>
2023-04-16 20:56:26 -0600

    portada

--- src/componentes/Inicio/index.js
+++ src/componentes/Inicio/index.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Portada from "../../images/inicio.jpeg"
+import { Link} from "react-router-dom";
 
 export const Inicio = () => {
   return (
-    <>
-      <h1 className="title">Inicio</h1>
-    </>
+    <div className="inicio">
+      <Link to= "/">
+        <h1>Inicio</h1>
+      </Link>
+      <Link to= "/productos">
+        <h1>Productos</h1>
+      </Link>
+      <img src={Portada} alt="inicio"/>
+    </div>
+  
   );
 };
