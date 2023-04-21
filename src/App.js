6f6c3c99328a9cd5d50b1de0c0357cb3e17637fe
IvonneAmezola <71673298+IvonneAmezola@users.noreply.github.com>
2023-04-20 20:31:32 -0600

    Data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Header } from "./componentes/Header";
 import "boxicons";
 import {BrowserRouter as Router} from "react-router-dom";
 import { Paginas } from "./componentes/Paginas";
+//import { DataProvider } from "./context/Dataprovider";
 
 function App() {
   return (
