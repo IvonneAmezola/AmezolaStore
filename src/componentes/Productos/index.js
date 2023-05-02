0a15bd20812c6c89ec81f70c6f2892c1fb4871dd
UrielLoreto <uriel.loreto8@gmail.com>
2023-05-01 23:06:47 -0600

    add validation

--- src/componentes/Productos/index.js
+++ src/componentes/Productos/index.js
@@ -1,33 +1,29 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 
-import {DataContext } from "../../context/Dataprovider";
-import {ProductoItem} from "./ProductoItem";
+import { DataContext } from "../../context/Dataprovider";
+import { ProductoItem } from "./ProductoItem";
 
 export const ProductosLista = () => {
+  const value = useContext(DataContext);
+  const [productos] = value.productos;
 
-    const value = useContext(DataContext)
-    const [productos]= value.productos
-
-    console.log(productos)
-
-    return (
-        <>
-            <h1 className="title">PRODUCTOS</h1>
-            <div className="productos">
-                {
-                    productos.map((producto) =>( 
-                        <ProductoItem 
-                        key= {producto.id} 
-                        id= {producto.id}
-                        title= {producto.title}
-                        price= {producto.price}
-                        image= {producto.image}
-                        category= {producto.category}
-                        cantidad= {producto.cantidad}
-                        />
-                    ))
-                }
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <h1 className="title">PRODUCTOS</h1>
+      <div className="productos">
+        {productos &&
+          productos.map((producto) => (
+            <ProductoItem
+              key={producto.id}
+              id={producto.id}
+              title={producto.title}
+              price={producto.price}
+              image={producto.image}
+              category={producto.category}
+              cantidad={producto.cantidad}
+            />
+          ))}
+      </div>
+    </>
+  );
+};
