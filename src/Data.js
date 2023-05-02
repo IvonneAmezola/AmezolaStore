5bdd662de633357275a3bf9dd418bdc8735bf68a
IvonneAmezola <71673298+IvonneAmezola@users.noreply.github.com>
2023-05-01 18:49:36 -0600

    video2

--- src/Data.js
+++ src/Data.js
@@ -46,7 +46,7 @@ const data = {
         title: "Juego de macetas cilíndricas medianas con base de madera",
         price: 950,
         image: require('./images/juegocilindro.jpeg'),
-        category: "nike",
+        category: "macetas",
         img1: " https://stockx-360.imgix.net/Nike-Dunk-Low-SP-Kentucky/Images/Nike-Dunk-Low-SP-Kentucky/Lv2/img",
         img2: ".jpg?auto=format,compress&q=90&updated_at=1606322330&w=1000",
         cantidad: 1
@@ -56,7 +56,7 @@ const data = {
         title: "Par de macetas bala chica con base de madera",
         price: 350,
         image: require('./images/parbalita.jpeg'),
-        category: "nike",
+        category: "macetas",
         img1: "https://stockx-360.imgix.net/Nike-Dunk-Low-Off-White-University-Red/Images/Nike-Dunk-Low-Off-White-University-Red/Lv2/img",
         img2: ".jpg?auto=format,compress&q=90&updated_at=1606321824&w=1000",
         cantidad: 1
--- src/context/Dataprovider.js
+++ src/context/Dataprovider.js
@@ -1,14 +1,20 @@
-/* import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect } from "react";
 import Data from '../Data.js';
 
 export const DataContext = createContext();
 
 export const DataProvider =(props) =>{
-    const [productos, setProductos] => useState([])
+    const [productos, setProductos] = useState([])
     
     useEffect(() =>{
-        const producto = Data
-        setProductos(producto)
+        const producto = Data.items
+        if (producto){
+            setProductos(producto)
+        }else{
+            setProductos(producto)
+        }
+
+        
     },[])
 
     const value ={
@@ -21,4 +27,4 @@ export const DataProvider =(props) =>{
         </DataContext.Provider>
     )
 
-}*/
+}
