929e3bf4a0930e56c63e646cb42c623a3b55406a
UrielLoreto <uriel.loreto8@gmail.com>
2023-05-01 23:06:19 -0600

    fix image

--- src/componentes/Productos/ProductoItem.js
+++ src/componentes/Productos/ProductoItem.js
@@ -1,43 +1,28 @@
 import React from "react";
-/*import IMG from "../../images/conicas.jpeg";
-import IMG2 from "../../images/juegopatonas.jpeg";
-import IMG3 from "../../images/individual.jpeg";
-import IMG4 from "../../images/juegobala.jpeg";
-import IMG5 from "../../images/juegocilindro.jpeg";
-import IMG6 from "../../images/parbalita.jpeg";*/
 
-export const ProductoItem = ({
-    id, 
-    title, 
-    price, 
-    image, 
-    category
-}) => {
-
-    console.log(image)
-    
-    return (
-        <><div className="producto">
-            <a href="#">
-                <div className="producto__img">
-                    <img src={image.default} alt={title} />
-                </div>
+export const ProductoItem = ({ id, title, price, image, category }) => {
+  return (
+    <>
+      <div className="producto">
+        <a href="#">
+          <div className="producto__img">
+            <img src={image} alt={title} />
+          </div>
+        </a>
+        <div className="producto__footer">
+          <h1> {title} </h1>
+          <p> {category} </p>
+          <p className="price">${price}</p>
+        </div>
+        <div className="buttom">
+          <button className="btn">Añadir al carrito</button>
+          <div>
+            <a href="#" className="btn">
+              Vista
             </a>
-            <div className="producto__footer">
-                <h1> {title} </h1>
-                <p> {category} </p>
-                <p className="price">${price}</p>
-            </div>
-            <div className="buttom">
-                <button className="btn">
-                    Añadir al carrito
-                </button>
-                <div>
-                    <a href="#" className="btn"> 
-                        Vista
-                    </a>
-                </div>
-            </div>
-        </div></>
-    )
-}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
