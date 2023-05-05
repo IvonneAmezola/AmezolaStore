c2762f032c59007e6395f85818ac1b942e68d6f9
IvonneAmezola <71673298+IvonneAmezola@users.noreply.github.com>
2023-05-05 10:13:58 -0600

    revision

--- src/componentes/Inicio/index.js
+++ src/componentes/Inicio/index.js
@@ -12,8 +12,9 @@ export const Inicio = () => {
       <br></br>
       <br></br>
       <footer>
+      <br></br>
         <center>
-          <h2> Para obtener más información sobre nuestros servicios sientase libre de ponerse en conctato con nosotros, en breve lo atenderemos.</h2>
+          <h2> Para obtener más información sobre nuestros servicios sientase libre de ponerse en contacto con nosotros, en breve lo atenderemos.</h2>
           <h3> Teléfono de Contacto: +52 (33)10970375, Visita nuestra tienda física en Av. Tonaltecas #84 Interior 11 </h3>
           <br></br>
           <p>Nota: Recuerde que todos nuestros productos se elaboran de manera artesanal
@@ -21,6 +22,7 @@ export const Inicio = () => {
             por lo que el tiempo de manufactura y entrega puede variar
             entre al menos 15 hasta 30 dias como maximo.</p>
         </center>
+        <br></br>
       </footer>
     </section>
   );
--- src/componentes/Productos/index.js
+++ src/componentes/Productos/index.js
@@ -24,6 +24,19 @@ export const ProductosLista = () => {
             />
           ))}
       </div>
+      <footer>
+      <br></br>
+        <center>
+          <h2> Para obtener más información sobre nuestros servicios sientase libre de ponerse en contacto con nosotros, en breve lo atenderemos.</h2>
+          <h3> Teléfono de Contacto: +52 (33)10970375, Visita nuestra tienda física en Av. Tonaltecas #84 Interior 11 </h3>
+          <br></br>
+          <p>Nota: Recuerde que todos nuestros productos se elaboran de manera artesanal
+            nuestros procesos son realizados a mano desde la produccion hasta el embalaje,
+            por lo que el tiempo de manufactura y entrega puede variar
+            entre al menos 15 hasta 30 dias como maximo.</p>
+        </center>
+        <br></br>
+      </footer>
     </>
   );
 };
