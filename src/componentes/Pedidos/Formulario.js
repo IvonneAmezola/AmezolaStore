9ac1adab675098f85a9c8897774a8211a730d18b
UrielLoreto <uriel.loreto8@gmail.com>
2023-05-16 20:30:37 -0600

    other formik

--- src/componentes/Pedidos/Formulario.js
+++ src/componentes/Pedidos/Formulario.js
@@ -1,114 +1,80 @@
-import React, { useState } from 'react';
-import { Formik, Form, ErrorMessage, Field } from 'formik';
+import React from "react";
+import { useFormik } from "formik";
+
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.firstName) {
+    errors.firstName = "Required";
+  } else if (values.firstName.length > 15) {
+    errors.firstName = "Must be 15 characters or less";
+  }
+
+  if (!values.lastName) {
+    errors.lastName = "Required";
+  } else if (values.lastName.length > 20) {
+    errors.lastName = "Must be 20 characters or less";
+  }
+
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email address";
+  }
+
+  return errors;
+};
 
 const Formulario = () => {
-    const [formularioEnviado, cambiarFormulario] = useState(false);
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="firstName">First Name</label>
+      <input
+        id="firstName"
+        name="firstName"
+        type="text"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.firstName}
+      />
+      {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
 
-    return (
-        <>
-            <Formik>
-                initialValues ={{
-                    nombre: '',
-                    correo: ''
+      <label htmlFor="lastName">Last Name</label>
+      <input
+        id="lastName"
+        name="lastName"
+        type="text"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.lastName}
+      />
+      {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
 
-                }}
-                validate ={(valores) => {
-                    let errores = {};
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.email}
+      />
+      {formik.errors.email ? <div>{formik.errors.email}</div> : null}
 
-                    if (!valores.nombre) {
-                        errores.nombre = 'Por favor ingresa tu nombre'
-                    } else if (!/^[a-zA-ZÀ-ÿ\s]{1,48}$/ - test(valores.nombre)) {
-                        errores.nombre = 'El nombre solo puede contener letras y espacios'
-                    }
-
-                    if (!valores.correo) {
-                        errores.correo = 'Por favor ingresa un correo electronico'
-                    } else if (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valores.correo)) {
-                        errores.correo = 'El correo solo puede contener, letras, numeros, puntos y guiones'
-                    }
-                    if (!valores.direccion) {
-                        errores.direccion = 'Por favor ingresa tu direccion de envio o si deseas recoger en sucursal'
-                    } else if (!/^[0-9a-zA-ZÀ-ÿ\s]{1,48}$/ - test(valores.nombre)) {
-                        errores.nombre = 'La direccion solo puede contener letras, numeros y espacios'
-                    }
-                    if (!valores.telefono) {
-                        errores.telefono = 'Por favor ingresa un numero telefonico a 10 digitos'
-                    } else if (!/^[0-9]+$/.test(valores.telefono)) {
-                        errores.correo = 'El telefono solo puede contener numeros'
-                    }
-                    return errores
-                }}
-                onSubmit = {(valores, { resetForm }) => {
-                    resetForm();
-                    console.log('Formulario enviado');
-                    cambiarFormularioEnviado(true);
-                    setTimeout(() => cambiarFormularioEnviado(false), 5000);
-                }}
-                {({errors}) => (
-
-
-                    <Form className="formulario" >
-                        <div>
-                            <label htmlFor="nombre">Nombre</label>
-                            <Field
-                                type="text"
-                                name="nombre"
-                                placeholder="Nombre"
-                                id="nombre"
-                            />
-                            <ErrorMessage name='nombre' component={(=>
-                            <div className='error'>{errors.name}</div>
-                            )} />
-                            
-                        </div>
-
-                        <div>
-                            <label htmlFor="correo">Correo</label>
-                            <Field
-                                type="email"
-                                name="correo"
-                                placeholder="correo@correo.com"
-                                id="correo"
-                            />
-                            <ErrorMessage name='correo' component={() =>
-                            (<div className='error'>{errors.correo}</div>
-                            )} />
-                        </div>
-                        <div>
-                            <label htmlFor="direccion">Direccion</label>
-                            <Field
-                                name="mensaje"
-
-                                placeholder="Direccion"
-                                id="direccion"
-                                
-                            />
-                            <p className='recoger'>Si desea recoger en la sucursal, solo ponga "recoger en sucursal" </p>
-                            <ErrorMessage name='direccion' component={() =>
-                            (<div className='error'>{errors.direccion}</div>
-                            )} />
-                            
-                        </div>
-                        <div>
-                            <label htmlFor="telefono">Telefono</label>
-                            <Field
-                                type="text"
-                                name="telefono"
-                                placeholder="Telefono"
-                                id="telefono"
-                            />
-                            <ErrorMessage name='telefono' component={() =>
-                            (<div className='error'>{errors.telefono}</div>
-                            )} />
-                        </div>
-
-                        <button type="submit">Enviar datos de pedido</button>
-                        {formularioEnviado && <p className='exito'>Formulario enviado con exito</p>}
-                    </Form>
-                )}
-            </Formik>
-        </>
-    );
-}
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
 export default Formulario;
